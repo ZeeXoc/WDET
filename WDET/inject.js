73e397eb80b9deee526a0fabc34967ad3abb6962
ZeeXoc <ZeeXoc@outlook.com>
2020-10-01 10:01:52 +0800

    0.0.2

--- WDET/inject.js
+++ WDET/inject.js
@@ -1,4 +1,6 @@
-if (typeof (window.WDET) == "undefined") {
+if (WIKIDOT){
+    alert("不是wikidot网站");
+}else if (typeof (window.WDET) == "undefined") {
     window.jq = jQuery;
     window.WDET = {
         ROOT: "http://localhost/public/WDET/",
@@ -10,13 +12,14 @@ if (typeof (window.WDET) == "undefined") {
             script.appendTo("head");
         },
         init: function () {
+            WIKIDOT.page.listeners.editClick();
             for (let script in this.scripts) {
                 this.inject(this.ROOT + WDET.scripts[script].url);
             }
         },
         scripts: {
             WSTP: {
-                name: "Wikidot Sandbox Tools Manager",
+                name: "Wikidot Sandbox Tools Panal",
                 short: "WSTP",
                 auther: "ZeeXoc",
                 homepage: "http://github.com/ZeeXoc",
--- WDET/scripts/WSTP.js
+++ WDET/scripts/WSTP.js
@@ -1,7 +1,102 @@
 if (WDET&&!WDET.panal){
     WDET.panal = {
-        init : function () {
-            jq("#wd-editor-toolbar-panel").remove();
+        actArea:jq("#wd-editor-toolbar-panel"),
+        init : function (){
+            this.actArea.empty();
+        },
+        source:{
+            css:" *{\n" +
+                "    padding: 0;\n" +
+                "    margin: 0;\n" +
+                "    }\n" +
+                "    ul{\n" +
+                "    list-style-type:none;\n" +
+                "    }\n" +
+                "    .triangle{\n" +
+                "\n" +
+                "    font-size: 1em;\n" +
+                "    color: white;\n" +
+                "    }\n" +
+                "    a{\n" +
+                "    text-decoration:none;\n" +
+                "    text-align: center;\n" +
+                "    font-weight: bold;\n" +
+                "\n" +
+                "    }\n" +
+                "    /*一级导航*/\n" +
+                "    ul.nav{\n" +
+                "    float: left;\n" +
+                "    border:1px solid gray;\n" +
+                "    margin-left: 10%;\n" +
+                "    margin-top:10px;\n" +
+                "    border-radius: 4px;\n" +
+                "    position: fixed;\n" +
+                "    }\n" +
+                "    ul.nav li{\n" +
+                "    float: left;\n" +
+                "    width: 9em;\n" +
+                "    background-color:yellowgreen;\n" +
+                "    }\n" +
+                "    ul.nav a{\n" +
+                "    display: block;\n" +
+                "    color: white;\n" +
+                "    line-height: 1.5em;\n" +
+                "    border-right: 1px solid white;\n" +
+                "    border-left: 1px solid white;\n" +
+                "    padding: 5px;\n" +
+                "    }\n" +
+                "\n" +
+                "    ul.nav a:hover,\n" +
+                "    ul.nav a:focus{\n" +
+                "    color: black;\n" +
+                "    background-color: #98FB98;\n" +
+                "    opacity: 0.5;\n" +
+                "    }\n" +
+                "    ul.nav li:first-child a{\n" +
+                "    border-left: 0;\n" +
+                "    border-bottom: 0;\n" +
+                "    }\n" +
+                "    ul.nav li:last-child a{\n" +
+                "    border-right: 0;\n" +
+                "    border-bottom: 0;\n" +
+                "    }\n" +
+                "    /*二级导航*/\n" +
+                "    ul.nav li ul{\n" +
+                "    width: 10em;\n" +
+                "    position: absolute;\n" +
+                "    left: -1000em;\n" +
+                "    }\n" +
+                "\n" +
+                "    ul.nav li:hover ul{\n" +
+                "    width: 10em;\n" +
+                "    left:auto;\n" +
+                "    }\n" +
+                "\n" +
+                "    ul.nav li ul a{\n" +
+                "    border-top: 1px solid white;\n" +
+                "    border-bottom: 1px solid white;\n" +
+                "    border-left: 0;\n" +
+                "    border-right: 0;\n" +
+                "\n" +
+                "    }\n" +
+                "    /*三级导航*/\n" +
+                "    ul.nav li:hover ul li ul{\n" +
+                "    width: 10em;\n" +
+                "    position: absolute;\n" +
+                "    left: -1000em;\n" +
+                "\n" +
+                "    }\n" +
+                "\n" +
+                "    ul.nav li:hover ul li:hover ul{\n" +
+                "    left: auto;\n" +
+                "    margin-left: 9.1em;\n" +
+                "    margin-top: -2.1em;\n" +
+                "    }\n" +
+                "    ul.nav li:hover ul li:hover ul.nav1{\n" +
+                "    left: auto;\n" +
+                "    margin-left: -9.1em;\n" +
+                "    margin-top: -2.1em;\n" +
+                "    }",
         }
     }
     WDET.panal.init();
